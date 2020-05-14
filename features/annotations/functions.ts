475db69f6cfdd633e26fc7bd7c98f088da618035
Adarsh Nanwani <adarsh.nanwani@sapiens.com>
2020-05-14 18:25:36 +0530

    Add annotation with object destructuring.

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -18,3 +18,22 @@ const logger = (message: string): void => {
 const throwError = (message: string): never => {
   throw new Error(message);
 }
+
+// Destructuring with Annotations
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny'
+};
+
+const logWeather = (forecast: { date: Date, weather: string }): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather)
+}
+
+// With ES2015
+const logWeather2 = ({ date, weather }: { date: Date, weather: string }): void => {
+  console.log(date);
+  console.log(weather)
+}
+
+logWeather(todaysWeather);
