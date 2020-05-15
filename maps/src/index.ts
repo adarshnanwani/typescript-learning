6d6fd1cf40c535dcc5b3b8a7b1a64b891ed08dca
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-15 15:35:03 +0530

    Add company marker.

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -3,7 +3,9 @@ import { Company } from './Company';
 import { CustomMap } from './CustomMap';
 
 const user = new User();
+const company = new Company();
 
 const map = new CustomMap('map');
 
 map.addUserMarker(user);
+map.addCompanyMarker(company);
