5de961ad9d13d3457c6656079367636271d5c83e
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-15 21:34:56 +0530

    Refactor with a better design.

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,33 +1,8 @@
-class Sorter {
-  constructor(public collection: number[] | string) {
-
-  }
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-
-        // If collection is an array of numbers
-        if (this.collection instanceof Array) { /* This is a type guard */
-          if (this.collection[j] > this.collection[j + 1]) {
-            const leftHand = this.collection[j];
-            this.collection[j] = this.collection[j + 1];
-            this.collection[j + 1] = leftHand;
-          }
-        }
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
 
 
-        // If collection is a string, do this logic instead
-        if (typeof this.collection === 'string') {
-
-        }
-      }
-    }
-  }
-}
-
-
-const sorter = new Sorter([10, 3, - 5, 0])
+const numbersCollection = new NumbersCollection([10, 3, - 5, 0]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(sorter.collection);
+console.log(numbersCollection.data);
