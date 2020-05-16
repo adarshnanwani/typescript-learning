75276d132ed550183c689ba41e92662203d4f0fa
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-16 19:24:39 +0530

    Add basic report with bad/non-ts code.

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -4,4 +4,12 @@ const matches = fs.readFileSync('original.csv', {
   encoding: 'utf-8'
 }).split("\n").map((row: string): string[] => row.split(","))
 
-console.log(matches)
+let manUnitedWins = 0;
+
+for (let match of matches) {
+  if ((match[1] === 'Man United' && match[5] == 'H') || (match[2] === 'Man United' && match[5] == 'A')) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games.`)
