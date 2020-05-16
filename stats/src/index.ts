af9266a1da4b35b73bb37d81e9ea23453ed2a995
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-16 23:05:59 +0530

    Add CsvFileReader class and read the file.

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,13 +1,18 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 
-const matches = fs.readFileSync('original.csv', {
-  encoding: 'utf-8'
-}).split("\n").map((row: string): string[] => row.split(","))
+const reader = new CsvFileReader('original.csv');
+reader.read();
 
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
 let manUnitedWins = 0;
 
-for (let match of matches) {
-  if ((match[1] === 'Man United' && match[5] == 'H') || (match[2] === 'Man United' && match[5] == 'A')) {
+for (let match of reader.data) {
+  if ((match[1] === 'Man United' && match[5] == MatchResult.HomeWin)
+    || (match[2] === 'Man United' && match[5] == MatchResult.AwayWin)) {
     manUnitedWins++;
   }
 }
