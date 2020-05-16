b2be8c6d4929c4b307f9a984b9c4b06757ed4152
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-16 18:10:20 +0530

    Use LinkedList with Sorter.

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 
 const numbersCollection = new NumbersCollection([10, 3, - 5, 0]);
@@ -12,3 +13,12 @@ console.log(numbersCollection.data);
 const strSorter = new Sorter(charactersCollection);
 strSorter.sort();
 console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(10);
+linkedList.add(3);
+linkedList.add(-5);
+linkedList.add(0);
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
