b3bb1173dcb1bb95580d8026fe9148c7f4442dc9
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-18 14:53:58 +0530

    Add method for trigger.

--- web/src/index.ts
+++ web/src/index.ts
@@ -2,7 +2,10 @@ import { User } from './models/User'
 
 const user = new User({ name: 'John', age: 25 });
 
-user.on('change', () => { });
-user.on('change', () => { });
-user.on('click', () => { });
+user.on('change', () => { console.log('change 1') });
+user.on('change', () => { console.log('change 2') });
+user.on('click', () => { console.log('click') });
 console.log(user)
+
+user.trigger('change');
+user.trigger('click');
