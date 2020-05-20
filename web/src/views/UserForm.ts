330620c901edf40236428ad2df05dae6eb8af914
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-20 14:05:14 +0530

    Update the UserFrom class.

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -5,8 +5,13 @@ export class UserForm extends View<User, UserProps> {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
-      'click:.set-name': this.onSetNameClick
+      'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick
     }
+  }
+
+  onSaveClick = (): void => {
+    this.model.save()
   }
 
   onSetAgeClick = (): void => {
@@ -25,12 +30,10 @@ export class UserForm extends View<User, UserProps> {
   template(): string {
     return `
       <div>
-        <h1>User Form</h1>
-        <div>Name: ${this.model.get('name')}</div>
-        <div>Age: ${this.model.get('age')}</div>
-        <input />
+        <input type="text" placeholder="${this.model.get('name')}" />
         <button class="set-name">Change Name</button>
         <button class="set-age">Set Random Age</button>
+        <button class="save-model">Save User</button>
       </div>
     `;
   }
