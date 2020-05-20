fe72c45c46378840429e9ee3739bd8b55b8d0a3c
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-20 12:37:56 +0530

    Enable strict mode with tsc --init and tsconfig.json

--- web/src/views/UserForm.ts
+++ web/src/views/UserForm.ts
@@ -25,9 +25,12 @@ export class UserForm {
   onSetNameClick = (): void => {
     const input = this.parent.querySelector('input');
 
-    const name = input.value;
+    if (input) {
+      const name = input.value;
 
-    this.model.set({ name })
+      this.model.set({ name })
+    }
+
 
   }
 
