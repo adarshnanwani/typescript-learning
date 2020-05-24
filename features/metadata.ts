9043fee7584f7c626d6e2abc9498996b8d6e07c8
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-24 13:53:35 +0530

    Decorators

--- features/metadata.ts
+++ features/metadata.ts
@@ -1,24 +1,52 @@
 import 'reflect-metadata';
 
-const plane = {
-  color: 'red'
-};
 
-// Define metadata for Objects
-Reflect.defineMetadata('note', 'hi there', plane);
-Reflect.defineMetadata('height', 10, plane);
+// /* WITH JS */
 
-const note = Reflect.getMetadata('note', plane);
-const height = Reflect.getMetadata('height', plane);
+// const plane = {
+//   color: 'red'
+// };
 
-console.log(note);
+// // Define metadata for Objects
+// Reflect.defineMetadata('note', 'hi there', plane);
+// Reflect.defineMetadata('height', 10, plane);
 
-console.log(height);
+// const note = Reflect.getMetadata('note', plane);
+// const height = Reflect.getMetadata('height', plane);
 
-// Define metadata for properties
+// console.log(note);
 
-Reflect.defineMetadata('note2', 'hi there2', plane, 'color');
+// console.log(height);
 
-const note2 = Reflect.getMetadata('note2', plane, 'color');
+// // Define metadata for properties
 
-console.log(note2);
+// Reflect.defineMetadata('note2', 'hi there2', plane, 'color');
+
+// const note2 = Reflect.getMetadata('note2', plane, 'color');
+
+// console.log(note2);
+
+
+// /* WITH TS */
+
+@printMetadata
+class Plane {
+  color: string = 'red';
+
+  @markFunction('123')
+  fly(): void {
+    console.log('vrrrrr');
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key)
+  }
+}
+
+function printMetadata(target: typeof Plane) {
+  for (let key in target.prototype) {
+    console.log(Reflect.getMetadata('secret', target.prototype, key))
+  }
+}
