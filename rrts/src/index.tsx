4bed628414a4e27f2bca56118d62bed4857c4407
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-24 22:05:55 +0530

    Add TS based state and props.

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,9 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends Component {
+interface AppProps {
+  color?: string;
+}
+
+interface AppState {
+  counter: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = { counter: 0 };
+  }
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>Hi There!</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
