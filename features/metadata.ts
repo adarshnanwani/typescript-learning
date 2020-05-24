70e40a70c0a7ad6347f09dbd6195bf008e91b73b
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-24 13:10:42 +0530

    Metadata for object properties.

--- features/metadata.ts
+++ features/metadata.ts
@@ -4,6 +4,7 @@ const plane = {
   color: 'red'
 };
 
+// Define metadata for Objects
 Reflect.defineMetadata('note', 'hi there', plane);
 Reflect.defineMetadata('height', 10, plane);
 
@@ -13,3 +14,11 @@ const height = Reflect.getMetadata('height', plane);
 console.log(note);
 
 console.log(height);
+
+// Define metadata for properties
+
+Reflect.defineMetadata('note2', 'hi there2', plane, 'color');
+
+const note2 = Reflect.getMetadata('note2', plane, 'color');
+
+console.log(note2);
