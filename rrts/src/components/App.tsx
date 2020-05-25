cc5652c67de64d1aae289b4dd9f520ace7609dc4
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-25 08:10:54 +0530

    Connect app to redux.

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers/';
 
-export class App extends React.Component {
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+
+export class _App extends React.Component<AppProps> {
+  componentDidMount() {
+    this.props.fetchTodos();
+  }
+
   render() {
     return <div>Hi there</div>;
   }
 }
+
+const mapStateToProps = (state: StoreState): { todos: Todo[] } => ({
+  todos: state.todos,
+});
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
