b6435378c6eed3540ec32a061a0f22a8d084337f
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2020-05-25 08:14:29 +0530

    Fetch data on click.{

--- rrts/src/components/App.tsx
+++ rrts/src/components/App.tsx
@@ -9,12 +9,23 @@ interface AppProps {
 }
 
 export class _App extends React.Component<AppProps> {
-  componentDidMount() {
+  onButtonClick = (): void => {
     this.props.fetchTodos();
+  };
+
+  renderList(): JSX.Element[] {
+    return this.props.todos.map((todo) => (
+      <div key={todo.id}>{todo.title}</div>
+    ));
   }
 
   render() {
-    return <div>Hi there</div>;
+    return (
+      <div>
+        <button onClick={this.onButtonClick}>Fetch</button>
+        {this.renderList()}
+      </div>
+    );
   }
 }
 
